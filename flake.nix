124a1edf35f0cbe0d621b5d2b7f12adb11a828c8
Deadbeef <ent3rm4n@gmail.com>
2024-08-17 12:04:39 +0800

    ugh flakes

--- flake.nix
+++ flake.nix
@@ -9,10 +9,18 @@
       pkgs = import nixpkgs { inherit system; };
     in
     {
-      devShells.default = with pkgs; pkgs.mkShell {
+      defaultPackage = with pkgs; (rust.packages.stable.rustPlatform).buildRustPackage {
+        pname = "nspire-leaveptt";
+        version = "0.1.0";
+        src = ./.;
+        nativeBuildInputs = [ pkg-config ];
+        buildInputs = [ libusb1 ];
+        cargoHash = "sha256-E6avbLtQ3WkaGvsp7r8tD4ntCPc0Mtd+JU5v+zP6weg=";
+      };
+      /*devShells.default = with pkgs; pkgs.mkShell {
         buildInputs = [ gcc pkg-config libusb1 ];
         # LIBCLANG_PATH = "${llvmPackages.libclang.lib}/lib";
-        /*shellHook = ''
+        shellHook = ''
             export BINDGEN_EXTRA_CLANG_ARGS="$(< ${stdenv.cc}/nix-support/libc-crt1-cflags) \
             $(< ${stdenv.cc}/nix-support/libc-cflags) \
             $(< ${stdenv.cc}/nix-support/cc-cflags) \
@@ -20,7 +28,7 @@
             ${lib.optionalString stdenv.cc.isClang "-idirafter ${stdenv.cc.cc}/lib/clang/${lib.getVersion stdenv.cc.cc}/include"} \
             ${lib.optionalString stdenv.cc.isGNU "-isystem ${stdenv.cc.cc}/include/c++/${lib.getVersion stdenv.cc.cc} -isystem ${stdenv.cc.cc}/include/c++/${lib.getVersion stdenv.cc.cc}/${stdenv.hostPlatform.config} -idirafter ${stdenv.cc.cc}/lib/gcc/${stdenv.hostPlatform.config}/${lib.getVersion stdenv.cc.cc}/include"} \
           "
-        '';*/
-      };
+        '';
+      };*/
     });
 }
